0fa29de25f8d57294a0599e95470e35315e23995
Tosinnijis1 <adenijitosin112@gmail.com>
2021-03-02 02:17:03 -0500

    build added

---
+++ build/precache-manifest.1edae8d50841e712b6de107e9817ce87.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75a34a6026a0edf9ca96d12feb30a069",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a1ee823f849e2939ef0",
+    "url": "/static/css/main.70568eae.chunk.css"
+  },
+  {
+    "revision": "699c2ce96e6082824036",
+    "url": "/static/js/2.8fdcc74a.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.8fdcc74a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a1ee823f849e2939ef0",
+    "url": "/static/js/main.b44a267b.chunk.js"
+  },
+  {
+    "revision": "d234c4965ad33a9f82f2",
+    "url": "/static/js/runtime-main.a31d20aa.js"
+  },
+  {
+    "revision": "edf88f12937763ea81267cd4f880513e",
+    "url": "/static/media/Game4x4.edf88f12.png"
+  },
+  {
+    "revision": "f38b56a794b71c5bda1b8c6664c62011",
+    "url": "/static/media/Game6x6.f38b56a7.png"
+  },
+  {
+    "revision": "ead505e3bae369a658f979a8fb8feeb6",
+    "url": "/static/media/cube4.ead505e3.gif"
+  },
+  {
+    "revision": "9b2359f717cc91c9c0347931b39a4038",
+    "url": "/static/media/games.9b2359f7.png"
+  },
+  {
+    "revision": "f309356acb4e753070c7d0e502c02765",
+    "url": "/static/media/omega-logo.f309356a.png"
+  },
+  {
+    "revision": "ed72175ad702f38f94fb6e02fdfcf77d",
+    "url": "/static/media/sudoku.ed72175a.png"
+  },
+  {
+    "revision": "597bb70b0eb5c290e1a51071d8846aa0",
+    "url": "/static/media/sudoku1.597bb70b.png"
+  },
+  {
+    "revision": "65144dc343881a4971d1d8dd9f359db5",
+    "url": "/static/media/tutorial-banner.65144dc3.png"
+  }
+]);
